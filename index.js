7906632fec88c5549a1428c8fafd5c8d4b692033
Zulaikha <108258939+aikaauwu@users.noreply.github.com>
2023-02-22 05:56:58 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,16 +4,18 @@ const { Client } = require('pg');
 const app = express();
 const port_number = process.env.PORT || 5000;
 
-const client = new Client({
+
+
+app.get('/', (req, res) => {
+  client.connect();
+
+  const client = new Client({
   connectionString: 'postgres://yadcxethtlqosn:20fdd8e07a709fae226a7c685df595d7162d04f8dd1081f731b3639b29ad334b@ec2-3-227-68-43.compute-1.amazonaws.com:5432/d2cm8tatb0l79l',
   ssl: {
     rejectUnauthorized: false
   }
 });
-
-app.get('/', (req, res) => {
-  client.connect();
-
+  
   client.query('SELECT * FROM resident', (err, result) => {
     if (err) throw err;
     res.send(result.rows);
